9711afb91b9fa33576d086d38f06c5107d65664a
amr <amrnooour@gmail.com>
2024-03-06 13:10:21 +0200

    Initial commit

---
+++ lib/features/splash/widgets/splash_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplashBody extends StatelessWidget {
+  const SplashBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/features/splash/splash.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: Splash(),
+    );
+  }
+}
+
